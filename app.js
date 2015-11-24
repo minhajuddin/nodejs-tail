16edbbffaa3bc7a77bd0e985de7e6f013608bd33
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2015-11-24 21:03:21 +0530

    save

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendfile('index.html');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+setInterval(function(){
+  io.emit("log", "Insane\n");
+}, 1000)
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
